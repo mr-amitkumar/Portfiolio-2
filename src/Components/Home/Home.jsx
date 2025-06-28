40fd58d296cec4afdec7c0c4e9374c61d9399d95
Amit Kumar Mahapatra <157622482+mr-amitkumar@users.noreply.github.com>
2025-06-28 16:58:10 +0530

    amit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Image from "../../assets/pics.jpg";
+import { MdArrowOutward } from "react-icons/md";
 const Home = () => {
     return (
         <div className="bg-gray-300 h-[90vh] sm:h-[88.7vh] text-white">
@@ -10,6 +11,8 @@ const Home = () => {
                         <strong className="text-green-600">Web Developer (Front-end developer)</strong> | Problem Solver | Quick Learner
                     </p>
                     <p className="text-gray-700">building the Frontend of Websites and <strong className="text-blue-600">web-applications</strong> that lead to the success of the overall product</p>
+
+                    <button className="bg-orange-500 flex justify-center align-middle px-1 py-2 rounded-md shadow-lg text-black font-semibold hover:bg-orange-400 sm:text-xl transition-all duration-300">Go To Projects <MdArrowOutward className="items-center ml-2 rounded-full  text-2xl sm:text-3xl p-1 border-black border-2"/></button>
 
                 </div>
                 <div className="border-2 p-2 rounded-full text-gray-600 shadow-md shadow-gray-600 border-gray-600 sm:mt-0 mt-4 sm:mb-0 mb-4 ">
